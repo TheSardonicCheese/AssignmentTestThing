f15d3d37e6ad677fd7177c2b193a31cf44a36136
Luke Stanley <1015748@student.sae.edu.au>
2019-06-19 17:14:08 +1000

    player and obstacles
    still facing downwards

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour 
+{
+	CharacterController controller;
+
+	public float Speed = 6.0f;
+	public float JumpSpeed = 8.0f;
+	public float Gravity =40.0f;
+
+	Vector3 MoveDirection = Vector3.zero;
+
+
+	void Start () 
+	{
+		controller = GetComponent<CharacterController>();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if(controller.isGrounded)
+		{
+			MoveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+			MoveDirection *= Speed;
+
+			if(Input.GetButton("Jump"))
+			{
+				MoveDirection.y = JumpSpeed;
+			}
+		}
+		else
+		{
+			MoveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+			MoveDirection *= Speed;
+
+			if(Input.GetButton("Jump"))
+			{
+				MoveDirection.y = JumpSpeed;
+			}
+		}
+
+		MoveDirection.y -= Gravity * Time.deltaTime;
+
+		controller.Move(MoveDirection * Time.deltaTime);
+
+		Vector3 LookDir = new Vector3(MoveDirection.x, 0, MoveDirection.z);
+		transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(MoveDirection), 0.15f);
+	}
+}
---
+++ Assets/Scripts/VariablesFunctions.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VariablesFunctions : MonoBehaviour 
+{
+
+	public int PlayerLives = 2;
+	float PlayerHealth = 100f;
+	bool PlayerAlive = true;
+	public string PlayerName = "Player";
+
+	void Start () 
+	{
+
+		PlayerLives++;
+		
+		if(PlayerLives == 3)
+		{
+			//do stuff
+		}
+		else if (PlayerLives == 2)
+		{
+			//do other stuff
+		}
+		else if (PlayerLives == 1)
+		{
+			//do thing
+		}
+		else
+		{
+			//gameover
+		}
+
+	}
+	
+	void Update () 
+	{
+		if(PlayerLives > 0)
+		{
+		DecreaseLives(1);
+		}
+
+		if(PlayerLives == 0)
+		{
+			Debug.Log("Game Over");
+		}
+	}
+
+	public void DecreaseLives(int IncLives)
+	{
+		PlayerLives-= IncLives;
+		Debug.Log(PlayerName + " has " + PlayerLives + " lives ");
+
+	}
+}
