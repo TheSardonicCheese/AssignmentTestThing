8c72fb0176837ae99d4b1992555850ee146b0ab0
Luke Stanley <1015748@student.sae.edu.au>
2019-06-19 23:17:26 +1000

    XP and Leveling System

---
+++ Assets/Scripts/XPandLvlUp.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class XPandLvlUp : MonoBehaviour 
+{
+public int CurrentLevel = 1;
+public float TotalXP = 0;
+public float XPRequired;
+
+	
+	void Update () 
+	{
+		 
+		if (CurrentLevel >= 5)
+		{
+			XPRequired = CurrentLevel*CurrentLevel + 3;
+		}
+		else
+		{
+			XPRequired = CurrentLevel * 3 + 4;
+		}
+
+		if(Input.GetKeyDown("x"))
+		{
+			//placeholder if statement until defeat enemy function works
+			TotalXP += 1.5f;
+			print("+1.5 xp");
+		}
+		if(TotalXP >= XPRequired)
+		{
+			CurrentLevel ++;
+			Debug.Log("Level Up!");
+		}
+	}
+}
