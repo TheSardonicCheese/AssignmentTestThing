a7205c845e4c9ccf91d1b24ed58ac1ce1b5bdde0
Luke <1015748@student.sae.edu.au>
2019-07-19 12:48:23 +1000

    Added battle stage
    game manager now battle manager, travel between overworld and battstage, also added grass script

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,165 +5,32 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    public List<GameObject> enemyList;
-
-    public List<GameObject> enemySpawnList;
-
-    
-
-    
-    public enum GameState
+    public enum Worlds
     {
-        notInCombat,
-        inCombat
+        OverWorld,
+        BattleStage
     }
-    public GameState gameState;
-
-    public enum CombatState
+    //void awke is called before void start on ANY  OBJECT
+    private void Awake()
     {
-        playerTurn,
-        enemyTurn,
-        victory,
-        loss
+        //this will make it so we can travel between scenes (good for keeping track of gameplay!)
+        DontDestroyOnLoad(this.gameObject);
     }
-    public CombatState combatState;
 
-    //objects for combat
-    public GameObject playerObj;
-    public GameObject enemyObj;
-
-    private bool doBattle = true;
-
-
-    void Start()
+    public void TravelToWorld(Worlds destination)
     {
-       
-        foreach (GameObject Enemy in GameObject.FindGameObjectsWithTag("Enemy"))
+        switch (destination)
         {
-            enemyList.Add(Enemy);
-        }
-
-    }
-
-    void Update()
-    {
-        if(doBattle)
-        {
-            //set who goes first by comparing speeds
-            StartCoroutine(battleGo());
-            doBattle = false;
-        }
-    }
-
-    public void DamageEnemies()
-    {
-        foreach (GameObject enemy in enemyList)
-        {
-            enemy.GetComponent<Stats>().health -= 10;
-        }
-    }
-
-    public void HealEnemies()
-    {
-        foreach (GameObject enemy in enemyList)
-        {
-            enemy.GetComponent<Stats>().health += 10;
-        }
-    }
-
-    public void RemoveEnemy(GameObject enemyToRemove)
-    {
-        enemyList.Remove(enemyToRemove);
-        enemySpawnList.Remove(enemyToRemove);
-    }
-
-    public void SpawnEnemy()
-    {
-        //Spawning an enemy using the size of the list as the maximum of the random range
-        enemyObj = Instantiate(enemySpawnList[Random.Range(0, enemySpawnList.Count)], transform);
-
-        Debug.Log(" An enemy appeard!");
-    }
-
-    public void CheckCombatState()
-    {
-        switch (combatState)
-        {
-            //Player Turn
-            case CombatState.playerTurn:
-                //decision - attack
-                //attack the enemy
-                BattleRound(playerObj, enemyObj);
-                //check if enemy is defeated
-                if (enemyObj.GetComponent<Stats>().isDefeated)
-                {
-                    //need to remove enemy
-                    combatState = CombatState.victory;
-                    break;
-                }
-                    
-                //next case, usually enemy turn
-                combatState = CombatState.enemyTurn;
+            case Worlds.OverWorld:
+                //load overworld
+                SceneManager.LoadScene("OverWorld");
                 break;
 
-
-                //Enemy Turn
-            case CombatState.enemyTurn:
-                //decision - attack
-                //attack the player
-                BattleRound(enemyObj, playerObj);
-                //check if player is defeated
-                if (playerObj.GetComponent<Stats>().isDefeated)
-                {
-                    //go to lose screen
-                    combatState = CombatState.loss;
-                    break;
-                }
-                //next case, usually player turn
-                combatState = CombatState.playerTurn;
+            case Worlds.BattleStage:
+                //go to battle scence
+                SceneManager.LoadScene("BattleStage");
                 break;
-
-            case CombatState.victory:
-                //we won
-                Debug.Log("Enemy Defeated");
-                RemoveEnemy(enemyObj);
-                SpawnEnemy();
-                break;
-
-            case CombatState.loss:
-                //we lose, restart
-                SceneManager.LoadScene("GameOver");
-                break;
-
-               
-
-
-                //Victory
-                //tell player they won
-                //end game
-
-                //loss
-                //tell player they lost
-                //restart game
         }
     }
 
-    public void BattleRound(GameObject attacker, GameObject defender)
-    {
-        //makes the fight happen
-        defender.GetComponent<Stats>().Attacked(attacker.GetComponent<Stats>().attack, Stats.StatusEffect.none);
-        Debug.Log(attacker.name +
-            " attacks " +
-            defender.name +
-            " for " +
-            (attacker.GetComponent<Stats>().attack - (attacker.GetComponent<Stats>().attack * (100/( defender.GetComponent<Stats>().defense + 100)))) + 
-            " damage");
-    }
-    IEnumerator battleGo()
-    {
-        CheckCombatState();
-        yield return new WaitForSeconds(1f);
-        doBattle = true;
-
-    }
 }
---
+++ Assets/Scripts/WildGrass.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WildGrass : MonoBehaviour
+{
+    private GameObject gameManager;
+
+    public bool isInGrass;
+
+
+
+    void Start()
+    {
+        gameManager = GameObject.FindGameObjectWithTag("GameManager");
+
+        RollDice();
+    }
+
+    void RollDice()
+    {
+        int diceRoll = Random.Range(1, 7);
+        Debug.Log(diceRoll);
+        if (diceRoll > 3 && isInGrass)
+        {
+            gameManager.GetComponent<GameManager>().TravelToWorld(GameManager.Worlds.BattleStage);
+        }
+        StartCoroutine(CheckTimer());
+    }
+
+    private void OnTriggerEnter(Collider col)
+    {
+        if(col.gameObject.tag == "Player")
+        {
+            isInGrass = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider col)
+    {
+        if(col.gameObject.tag == "Player")
+        {
+            isInGrass = false;
+        }
+    }
+    IEnumerator CheckTimer()
+    {
+        yield return new WaitForSeconds(2);
+        RollDice();
+    }
+}
